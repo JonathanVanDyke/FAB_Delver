a71524a3d859e9fe827e2a9ab72f76f37e99f4dc
Jonathan <jfv213@gmail.com>
2021-11-16 19:54:35 -0500

    Remove picture write to repo

--- src/camera.py
+++ src/camera.py
@@ -21,9 +21,9 @@ def run_camera():
           break
       elif k % 256 == 32:
           # SPACE pressed
-          img_name = "opencv_frame_{}.png".format(img_counter)
-          cv2.imwrite(img_name, frame)
-          print("{} written!".format(img_name))
+          # img_name = "opencv_frame_{}.png".format(img_counter)
+          # cv2.imwrite(img_name, frame)
+          # print("{} written!".format(img_name))
           img_counter += 1
 
           return frame
